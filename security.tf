dbabdeebb600fcdbebe726b2745627a56d6fc97f
unknown <max.thibaut164@gmail.com>
2018-06-20 09:45:05 +0200

    security modification

--- security.tf
+++ security.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "public-sg" {
     description = "Sample security group"
 
     ingress {
-        from_port = [80]
+        from_port = 80
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
