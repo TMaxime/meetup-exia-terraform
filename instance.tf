23fb2ff3b592bc6ef9f335640332fe1de5d099c1
unknown <max.thibaut164@gmail.com>
2018-06-20 09:40:57 +0200

    meet best practices

--- instance.tf
+++ instance.tf
@@ -1,3 +1,20 @@
+variable "ami" {
+    description = "AMI that will be used by both instances"
+}
+variable "instance_type" {
+    description = "Key pair name"
+}
+variable "key_pair" {
+    description = "Type of instance"
+}
+variable instance_tag_name_1 {
+    description = "The tag name of the first instance"
+}
+variable instance_tag_name_2 {
+    description = "The tag name of the second instance"
+}
+
+
 resource "aws_eip_association" "eip-1" {
     instance_id = "${aws_instance.instance-1.id}"
     allocation_id = "eipalloc-09c08b100e10c7bff"
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,31 @@
+variable "cidrs" {
+    description = "VPC cidr block"
+}
+variable "availability_zone" {
+    description = "Default vailability zone"
+}
+
+variable "vpc_tag_name" {
+    description = "Tag name of the VPC"
+}
+
+variable "subnet_tag_name" {
+    description = "Tag name of the subnet"
+}
+
+variable "rtb_cidr" {
+    description = "Route table cidr"
+}
+
+variable "rtb_tag_name" {
+    description = "Tag name of the route table"
+}
+
 resource "aws_vpc" "main" {
     cidr_block = "${var.cidrs}"
 
     tags {
-        Name = "MyVPC"
+        Name = "${var.vpc_tag_name}"
     }
 }
 
@@ -10,10 +33,9 @@ resource "aws_subnet" "public_subnet" {
     vpc_id = "${aws_vpc.main.id}"
     availability_zone = "${var.availability_zone}"
     cidr_block = "${var.cidrs}"
-    map_public_ip_on_launch = true
 
     tags {
-        Name = "MySubnet"
+        Name = "${var.subnet_tag_name}"
     }
 }
 
@@ -25,12 +47,12 @@ resource "aws_route_table" "public_route" {
     vpc_id = "${aws_vpc.main.id}"
 
     route {
-        cidr_block = "0.0.0.0/0"
+        cidr_block = "${var.rtb_cidr}"
         gateway_id = "${aws_internet_gateway.my_ig.id}"
     }
 
     tags {
-        Name = "Public route table"
+        Name = "${var.rtb_tag_name}"
     }
 }
 
