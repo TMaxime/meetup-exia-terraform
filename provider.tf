8926977e87552718ed0955eff66d72f8a1ca63e9
unknown <max.thibaut164@gmail.com>
2018-06-20 09:42:34 +0200

    add provider variable

--- provider.tf
+++ provider.tf
@@ -1,5 +1,15 @@
+variable "access_key" {
+    description = "IAM account access key"
+}
+variable "secret_key" {
+    description = "IAM account secret key"
+}
+variable "region" {
+    description = "AWS region"
+}
+
 provider "aws" {
-    access_key = "my_access_key"
-    secret_key = "my_secret_key"
-    region = "my_region"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "${var.region}"
 }
